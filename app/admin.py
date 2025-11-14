9ccdbd0ae41c5ba5ca31d63fc81b5c398ffa093a
ASHUTOSH KHILAR <ashutoshkhilar5@gmail.com>
2025-11-15 00:12:26 +0530

    Update admin site header and pagination settings

--- app/admin.py
+++ app/admin.py
@@ -8,13 +8,13 @@ class CustomWebpage(admin.ModelAdmin):
     list_display=['topic_name','name','url','email','pk']
     list_display_links=['name','pk']
     list_editable=['email']
-    list_per_page=3
+    list_per_page=4
     search_fields=['name']
     list_filter=['topic_name']
     
-admin.site.site_header = "Django Topic Database"
+admin.site.site_header = "Django Sports Topic Database"
 admin.site.site_title = "Admin Portal"
-admin.site.index_title = "Ashu"
+admin.site.index_title = "Sports"
 
 admin.site.register(Topic)
 admin.site.register(WebPage,CustomWebpage)
